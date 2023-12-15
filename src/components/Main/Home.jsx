49f52cf976884558745fb2daf215b0f7a1bbe85d
sammymarks <sammy.marks@gmail.com>
2023-12-15 15:52:18 -0600

    updated homepage

--- src/components/Main/Home.jsx
+++ src/components/Main/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { useState } from 'react'
-import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button  } from 'reactstrap'
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button, Table  } from 'reactstrap'
+import { Link, Route, Routes } from 'react-router-dom'
+
 
 export default function Home () {
 
@@ -9,10 +11,11 @@ export default function Home () {
             <Card body className="text-center" style={{ 
                     width: '100vw', 
                     backgroundColor: 'transparent', 
-                    border: 'none' 
+                    border: 'none',
+                    margin: 'none' 
             }}>
                 <CardBody>
-                    <CardTitle tag="h3">
+                    <CardTitle tag="h4">
                         Welcome to Safe Streets Survey!
                     </CardTitle>
                     <CardSubtitle 
@@ -21,14 +24,57 @@ export default function Home () {
                     >
                         Helping improve neighborhood access one picture at a time
                     </CardSubtitle>
+
                     <CardTitle tag="h5">
                         How does this work?
                     </CardTitle>
-                    <CardText>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                    </CardText>
-                    <Button>
-                        Button
+                    <Table borderless responsive style={{textAlign:'left'}} >
+                        <tbody>
+                            <tr>    
+                                <th scope="row">
+                                    1
+                                </th>
+                                <td>
+                                    Browse Projects to see all of the impactful projects happening in the community
+                                </td>
+                            </tr>    
+                            <tr>
+                                <th scope="row">
+                                    2
+                                </th>
+                                <td>
+                                Login with secure authentication
+                                </td>
+                            </tr>    
+                            <tr>
+                                <th scope="row">
+                                    3
+                                </th>
+                                <td>
+                                Join a Project
+                                </td>
+                            </tr>    
+                            <tr>
+                                <th scope="row">
+                                    4
+                                </th>
+                                <td>
+                                Submit a Ticket with location, images, and specific feedback
+                                </td>
+                            </tr>    
+                            <tr>
+                                <th scope="row">
+                                    5
+                                </th>
+                                <td>
+                                Hear back from your local organizer on what is being done to improve the community!
+                                </td>
+                            </tr>    
+
+                        </tbody>
+                    </Table>
+                    <Button className="home-button">
+                        <Link to="/projects/">Browse Projects</Link>
                     </Button>
                 </CardBody>
             </Card>
